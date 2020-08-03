063bbb7e54fd1182833b5e68643a7436a659a3c2
tiantian <1500846601@qq.com>
2020-08-03 21:54:57 +0800

    Updates

---
+++ precache-manifest.7a756c56c6a54b089fbd213af808f1b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cfd40bfc6c836926752b3b42910f143",
+    "url": "/actual-project/react/potapo/index.html"
+  },
+  {
+    "revision": "ed06956d75b4c4780b52",
+    "url": "/actual-project/react/potapo/static/css/2.8a731d57.chunk.css"
+  },
+  {
+    "revision": "57cd1fa9ef98ddcb06fc",
+    "url": "/actual-project/react/potapo/static/css/main.8455e325.chunk.css"
+  },
+  {
+    "revision": "ed06956d75b4c4780b52",
+    "url": "/actual-project/react/potapo/static/js/2.0fde05e1.chunk.js"
+  },
+  {
+    "revision": "d11a426366fe95fb0db6a00b1c810ec4",
+    "url": "/actual-project/react/potapo/static/js/2.0fde05e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57cd1fa9ef98ddcb06fc",
+    "url": "/actual-project/react/potapo/static/js/main.9b33b8b4.chunk.js"
+  },
+  {
+    "revision": "13f391a0f4a51921bf20",
+    "url": "/actual-project/react/potapo/static/js/runtime-main.43490cf9.js"
+  }
+]);
