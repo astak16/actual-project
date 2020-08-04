50a8f7eff4f43aad97024cbaafdafeaf8fec20f9
tiantian <1500846601@qq.com>
2020-08-04 22:29:35 +0800

    button 完成

--- react/ui-wheel/src/App.tsx
+++ react/ui-wheel/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, {ButtonSize, ButtonType} from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {/*<Button> Hello </Button>*/}
+        <Button disabled>Disabled Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Hello</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Hello</Button>
+        <Button btnType={ButtonType.Link} href='http://www.baidu.com' disabled>Disabled Baidu</Button>
+        <Button btnType={ButtonType.Link} href='http://www.baidu.com' >Baidu</Button>
+      </div>
+
     </div>
   );
 }
