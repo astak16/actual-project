dc0f2c128e851eff49cef2d2f70ab7383a44a393
tiantian <1500846601@qq.com>
2020-10-18 21:05:29 +0800

    项目初始化

--- react/bicycle-manager/src/index.js
+++ react/bicycle-manager/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import Admin from "./admin";
-import {Router} from "react-router-dom";
 
 ReactDOM.render(
-  // <React.StrictMode>
-    <Router />,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
